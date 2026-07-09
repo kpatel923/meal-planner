3af4c31ecb2fefda737c03d79c8a1ab5cd089ba9
Sagar <kandarp.patel923@gmail.com>
2026-07-09 17:26:24 -0400

    Error

--- src/pages/PlannerPage.jsx
+++ src/pages/PlannerPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useEffect } from 'react'
+import { useState, useMemo, useEffect, useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useMeals } from '../hooks/useMeals'
 import { usePlans } from '../hooks/usePlans'
